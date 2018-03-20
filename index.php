6ad6b02b510a617056d436df68d0ae44da617491
James Richardson <3richj71@solent.ac.uk>
2018-03-20 03:31:13 +0000

    First Commit of template

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+<?php require_once "parts/header.php" ?>
+<h1 class="title">Home Page</h1>
+    <div class="container">
+        <div class="box offset-3-left">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+        <div class="box offset-6-both">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+        <div class="box offset-3-right">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+        <div class="box offset-8-left">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+        <div class="box offset-2-both">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+        <div class="box offset-2-right">
+            <div class="box-content-container">
+                <h2 class="box-title">Title Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad aliquid aut blanditiis enim id laborum magni molestias
+                    officia optio praesentium quas quidem recusandae,
+                    similique sit temporibus totam ut vero voluptate.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,48 @@
+<header>
+    <img src="http://via.placeholder.com/960x150/ccc" alt="">
+    <nav>
+        <ul class="nav-container">
+            <li class="dropdown">
+                <span>CDA400</span>
+                <ul class="dropdown-content">
+                    <li><a href="">Session 1</a></li>
+                    <li><a href="">Session 2</a></li>
+                    <li><a href="">Advanced 2</a></li>
+                    <li><a href="">Session 3</a></li>
+                    <li><a href="">Session 4</a></li>
+                    <li><a href="">Advanced 4</a></li>
+                    <li><a href="">Session 5</a></li>
+                    <li><a href="">Session 6</a></li>
+                    <li><a href="">Mock TCA</a></li>
+                    <li><a href="">Session 8</a></li>
+                    <li><a href="">Session 9</a></li>
+                    <li><a href="">Session 12</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <span>CDA401</span>
+                <ul class="dropdown-content">
+                    <li>Session 1</li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <span>CDA400</span>
+                <ul class="dropdown-content">
+                    <li>Session 1</li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <span>CDA400</span>
+                <ul class="dropdown-content">
+                    <li>Session 1</li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <span>CDA400</span>
+                <ul class="dropdown-content">
+                    <li>Session 1</li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
